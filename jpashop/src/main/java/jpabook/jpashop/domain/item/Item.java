5b040dde86a1c85f83d308caed2d8071ba657f74
Gamddalki <hannahk01@naver.com>
2023-02-06 17:28:30 +0900

    Add Stock Logic to Item.java

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,16 @@ public abstract class Item {
     private int stockQuantity;
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //비즈니스 로직 (재고 관리)
+    public void addStock(int quantity){
+        this.stockQuantity+=quantity;
+    }
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity-quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("재고가 부족합니당");
+        }
+        this.stockQuantity=restStock;
+    }
 }
